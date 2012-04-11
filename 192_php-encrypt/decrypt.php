fc545c657975ebb37dff34b0f15e4b894bce32ed
Kalim Fleet <kalim.fleet@gmail.com>
2012-04-11 09:44:38 -0400

    192 branch commit

--- 192_php-encrypt/decrypt.php
+++ 192_php-encrypt/decrypt.php
@@ -8,7 +8,7 @@ $key = 'White Glow Silver Yellow Penny';
 // $string = ' string to be encrypted '; // note the spaces
 $string = $_POST['string']; // note the spaces
 
-
+//	//
 if($string){
 // $encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $string, MCRYPT_MODE_CBC, md5(md5($key))));
 // $decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($encrypted), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
