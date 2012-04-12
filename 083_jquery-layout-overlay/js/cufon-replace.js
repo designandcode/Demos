5597a42e1fdcedfab97f4f3c12e495586209fc58
Kalim Fleet <kalim.fleet@gmail.com>
2012-04-11 21:21:49 -0400

    initial commit

---
+++ 083_jquery-layout-overlay/js/cufon-replace.js
@@ -0,0 +1,5 @@
+Cufon.replace('#header ul li a', { fontFamily: 'Myriad Pro' });
+Cufon.replace('#header .indent1', { fontFamily: 'Myriad Pro l' });
+Cufon.replace('h2 span', { fontFamily: 'Swis721 Lt BT' })
+Cufon.replace('.title-1', { fontFamily: 'Swis721 Lt BT' })
+Cufon.replace('h3', { fontFamily: 'Myriad Pro b' , textShadow: '#f7f7f4 1px 1px' })
